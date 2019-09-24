9bda09a3560a63d71109f2073605a964083da5d7
Lucas Ponte <luckpontecorreia@gmail.com>
2019-09-24 08:26:17 -0300

    ex. 1247 - Coastal Guard

---
+++ src/coast.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int distance, fugSpeed, grdSpeed;
+	int i;
+
+	while(scanf("%d %d %d",&distance,&fugSpeed,&grdSpeed)!=EOF)
+	{
+		if(fugSpeed > grdSpeed)
+			printf("N\n");
+		else
+		{
+			if((12/fugSpeed) <= (distance/grdSpeed))
+				printf("N\n");
+			else
+				printf("S\n",(12/fugSpeed),(distance/grdSpeed));
+		}
+	}
+	return 0;
+}
